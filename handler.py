990e888dd48b8e7dce4bfdbef651f09a0cae9b14
Павел Яковлев <pavlik.yakovlev.2004@mail.ru>
2020-11-13 15:07:14 +0700

    made nothink

--- handler.py
+++ handler.py
@@ -9,16 +9,23 @@ def webhook(event, context):
     }
 
 
+state = 0
+
+
 def handler_function(req_mess):
+    global state
     buttons = []
     end_session = False
     message = ''
     session = req_mess['session']
     request = req_mess['request']
+    print(request)
 
-    if session['new']:
-        text = 'Вы на тренажере Маруся, поздравляю, выберите тип тренировок'
-        buttons = [button('')],
+    if session['new'] or state == 0:
+
+        message = 'Вы на тренажере Маруся, поздравляю, выберите тип тренировок'
+        buttons = [button('Разовая тренировка'), button('Комплекс на день')]
+        state = 1
 
     response_message = {
         "response": {
@@ -27,11 +34,13 @@ def handler_function(req_mess):
             "buttons": buttons,
             "end_session": end_session
         },
-        "session": {derived_key: req_mess['session'][derived_key] for derived_key in ['session_id', 'user_id', 'message_id']},
+        "session": {derived_key: req_mess['session'][derived_key] for derived_key in
+                    ['session_id', 'user_id', 'message_id']},
         "version": req_mess['version']
     }
     print(response_message)
     return response_message
 
+
 def button(title):
-    return {"title":title}
+    return {"title": title}
