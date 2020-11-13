2da5c2b1e5b4c5f865b19d008d995f64536346ea
Павел Яковлев <pavlik.yakovlev.2004@mail.ru>
2020-11-13 14:34:24 +0700

    add function handler and button func

--- handler.py
+++ handler.py
@@ -3,18 +3,35 @@ import json
 
 def webhook(event, context):
     request_message = json.loads(event['body'])
-    derived_session_fields = ['session_id', 'user_id', 'message_id']
-    response_message = {
-        "response": {
-            "text": request_message['request']['original_utterance'],
-            "tts": request_message['request']['original_utterance'],
-            "end_session": False
-        },
-        "session": {derived_key: request_message['session'][derived_key] for derived_key in derived_session_fields},
-        "version": request_message['version']
-    }
-
     return {
         "statusCode": 200,
-        "body": json.dumps(response_message)
+        "body": json.dumps(handler_function(request_message))
     }
+
+
+def handler_function(req_mess):
+    buttons = []
+    end_session = False
+    message = ''
+    session = req_mess['session']
+    request = req_mess['request']
+
+    if session['new']:
+        text = 'Вы на тренажере Маруся, поздравляю, выберите тип тренировок'
+        buttons = [button('')],
+
+    response_message = {
+        "response": {
+            "text": message,
+            "tts": message,
+            "buttons": buttons,
+            "end_session": end_session
+        },
+        "session": {derived_key: req_mess['session'][derived_key] for derived_key in ['session_id', 'user_id', 'message_id']},
+        "version": req_mess['version']
+    }
+    print(response_message)
+    return response_message
+
+def button(title):
+    return {"title":title}
